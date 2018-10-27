bcff527c9cbb7df91515c46e071cba3b8c39d240
Martin Białkowski <embe2sc@gmail.com>
2018-10-27 19:40:48 +0200

    Added Db Context

---
+++ BlogPost.WebApi/BlogPost.Core/Entities/BlogPostContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogPost.Core.Entities
+{
+    public class BlogPostContext: DbContext
+    {
+        public BlogPostContext(DbContextOptions<BlogPostContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
